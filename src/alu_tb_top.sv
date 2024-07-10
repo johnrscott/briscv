60117c8dd6c0723e97069244bd69a58e9969b5b4
John Scott <johnrscott0@gmail.com>
2024-07-11 00:11:09 +0100

    More work on UVM

--- src/alu_tb_top.sv
+++ src/alu_tb_top.sv
@@ -1,14 +1,55 @@
 `include "alu.sv"
 `include "alu_if.sv"
+`include "uvm_macros.svh"
+
+import uvm_pkg::*;
+
+class alu_transaction extends uvm_sequence_item;
+   rand bit [31:0] a;
+   rand bit [31:0] b;
+   bit [2:0]	   r;
+   
+   function new(string name = "");
+      super.new(name);
+   endfunction
+   
+   `uvm_object_utils_begin(alu_transaction)
+      `uvm_field_int(a, UVM_ALL_ON)
+      `uvm_field_int(b, UVM_ALL_ON)
+      `uvm_field_int(r, UVM_ALL_ON)
+   `uvm_object_utils_end
+   
+endclass
+
+class alu_sequence extends uvm_sequence#(alu_transaction);
+   `uvm_object_utils(alu_sequence)
+   
+   function new(string name = "");
+      super.new(name);
+   endfunction
+   
+   task body();
+      alu_transaction alu_tx;
+      
+      repeat(15) begin
+         alu_tx = alu_transaction::type_id::create();
+	 
+         start_item(alu_tx);
+         assert(alu_tx.randomize());
+         finish_item(alu_tx);
+      end
+   endtask
+endclass // alu_sequence
+
+typedef uvm_sequencer#(alu_transaction) alu_sequencer;
 
 module alu_tb_top;
-   import uvm_pkg::*;
    
    // Interface declaration
    alu_if vif();
    
    // Connects the Interface to the DUT
-   alu dut(.a(vif.a), .b(vif.b), .alu_op(vif.alu_op), .r(vif.r), .zero(zero));
+   alu dut(.a(vif.a), .b(vif.b), .alu_op(vif.alu_op), .r(vif.r), .zero(vif.zero));
    
    initial begin
       
@@ -20,9 +61,9 @@ module alu_tb_top;
       
       // Executes the test
       run_test();
-   
+      
    end
- 
+   
    // Variable initialization
    initial begin
       vif.sig_clock = 1'b1;
